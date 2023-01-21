5826055d6a93fe5198aac166847714a112d0091b
Lindsay <112891882+linhardie94@users.noreply.github.com>
2023-01-21 13:18:20 +0200

    Update _app.js

--- my-first-next-app/pages/_app.js
+++ my-first-next-app/pages/_app.js
@@ -4,21 +4,21 @@ import Script from "next/script";
 export default function MyApp({ Component, pageProps }) {
   return (
   <>
+    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-MDR9Y99KT2"/>
     <Script
-      strategy = 'lazyOnload'
-      src={`https://www.googletagmanager.com/gtag/js?id=G-MDR9Y99KT2`}
-    />
-
-    <Script 
-      strategy='lazyOnload'>
-        {
-          `window.dataLayer = window.dataLayer || [];
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
-          gtag('config', 'G-MDR9Y99KT2');`
-        }
-    </Script>
+          gtag('config', 'G-MDR9Y99KT2', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
     <Component {...pageProps} />
   </>
   )

bf10a0e9a28ed7cd44b97fdc794c9fd4b9d5791e
Lindsay <112891882+linhardie94@users.noreply.github.com>
2023-01-21 11:59:49 +0200

    Update _app.js

--- my-first-next-app/pages/_app.js
+++ my-first-next-app/pages/_app.js
@@ -1,6 +1,25 @@
-import './styles.css'
+import './styles.css';
+import Script from "next/script";
 
-// This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Script
+      strategy = 'lazyOnload'
+      src={`https://www.googletagmanager.com/gtag/js?id=G-MDR9Y99KT2`}
+    />
+
+    <Script 
+      strategy='lazyOnload'>
+        {
+          `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MDR9Y99KT2');`
+        }
+    </Script>
+    <Component {...pageProps} />
+  </>
+  )
 }
